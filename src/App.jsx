ab220fc6a159ec1e5fe4aa77651ce8f60f53aa41
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-05 15:36:14 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/*--------------------components--------------------*/
+import Navbar from './components/Navbar';
+import Index from './components/Index';
+import Card from './components/Card';
 
+import AgregarContacto from './components/AgregarContacto';
+import DetalleContacto from './components/DetalleContacto';
+import EditarContacto from './components/EditarContacto';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route index> <Index /> </Route>
+          <Route path="/agregarContacto"> <AgregarContacto /> </Route>
+          <Route path="/contacto/:id"> <DetalleContacto /> </Route>
+          <Route path="/contacto/:id/editar"> <EditarContacto /> </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
