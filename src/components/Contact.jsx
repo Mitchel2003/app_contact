464149c47a2b13c408c63c216dc11d642f13987d
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 11:37:03 -0500

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,1 +1,9 @@
-
+import '../styles/Contact.css';
+export default ({ name, phone }) => {
+    return (
+        <article className="contact">
+            <h2>{name}</h2>
+            <p>Telefono: {phone}</p>
+        </article>
+    )
+}
