cee0c77a67b4653b67e79c9b5fb3acd6ebf92ef1
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 11:17:21 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ export default function App(){
     return (
         <BrowserRouter>
           <Navbar />
-            <main>
-              <Routes>
-                <Route index element={ <Index /> } />
-                <Route path="/agregarContacto" element={<AddContact />} />
-                <Route path="/contacto/:id" element={<DetailsContact />} />
-                <Route path="/contacto/:id/editar" element={<SetContact />} />
-              </Routes>
-            </main>
+          <main>
+            <Routes>
+              <Route index element={ <Index /> } />
+              <Route path="/agregarContacto" element={<AddContact />} />
+              <Route path="/contacto/:id" element={<DetailsContact />} />
+              <Route path="/contacto/:id/editar" element={<SetContact />} />
+            </Routes>
+          </main>
         </BrowserRouter>
     )
 }
