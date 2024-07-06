b58acf70f14c6759d64234f4454b20c19e258bea
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 11:09:05 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 import Index from './components/pages/Index';
-import AgregarContacto from './components/pages/AgregarContacto';
-import DetalleContacto from './components/pages/DetalleContacto';
-import EditarContacto from './components/pages/EditarContacto';
+import SetContact from './components/pages/SetContact';
+import AddContact from './components/pages/AddContact';
+import DetailsContact from './components/pages/DetailsContact';
 
 export default function App(){
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route index element={ <Index /> } />
-          <Route path="/agregarContacto" element={<AgregarContacto />} />
-          <Route path="/contacto/:id" element={<DetalleContacto />} />
-          <Route path="/contacto/:id/editar" element={<EditarContacto />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+          <Navbar />
+            <main>
+              <Routes>
+                <Route index element={ <Index /> } />
+                <Route path="/agregarContacto" element={<AddContact />} />
+                <Route path="/contacto/:id" element={<DetailsContact />} />
+                <Route path="/contacto/:id/editar" element={<SetContact />} />
+              </Routes>
+            </main>
+        </BrowserRouter>
+    )
 }
