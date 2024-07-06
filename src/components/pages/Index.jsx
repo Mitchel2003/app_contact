b78707a7c119bf61576cad4c938ea2877a78a9c4
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 10:10:58 -0500

    commit

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,1 +1,24 @@
+import { useState, useEffect } from 'react';
+import ListContacts from '../ListContacts';
+import '../../styles/Index.css';
 
+export default () => {
+    const [contacts, setContacts] = useState([]);
+    useEffect(() => { loadContacts() }, []);
+
+    const loadContacts = async () => {
+        const res = await fetch(...);
+        const json = await res.json();
+        setContacts(json);
+    }
+
+    return (
+      <>
+        <h2> Contacts </h2>
+        { contacts.length > 0 ?
+            (<ListContacts contacts={contacts} />) :
+            (<div className={center}> <p>Nothing found</p> </div>)
+        }
+      </>
+    )
+}
