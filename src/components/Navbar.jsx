8d01e4ef47266f998b22d3caeefeb61b0b53534d
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 15:20:28 -0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,13 @@
+import { Link } from 'react-router-dom';
+import './styles/Navbar.css';
 
+export default () => {
+    return (
+        <nav className="navigation">
+            <Link to="/"> App contactos </Link>
+            <Link to="/agregarContacto">
+                <button className="button primary"> Agregar contacto </button>
+            </Link>
+        </nav>
+    )
+}
