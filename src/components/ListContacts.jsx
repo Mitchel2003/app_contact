f968e1668511a577b538201850e9786c882639bb
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-06 11:20:24 -0500

    commit

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -5,7 +5,7 @@ export default ({ contacts }) => {
     return (
         <section className="contacts">
             { contacts.map(e => (
-                <Link to={`/contact/${e.id}`}>
+                <Link to={`/contacto/${e.id}`}>
                     <Contact key={e.id} {...e} />
                 </Link>
               )) }
