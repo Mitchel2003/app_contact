2f282b3c30db460981d64f075739e4aa9c07627a
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-07 16:34:43 -0500

    commit

--- src/components/pages/AddContact.jsx
+++ src/components/pages/AddContact.jsx
@@ -4,7 +4,7 @@ import '../styles/AddContact.css';
 
 export default () => {
     const navigate = useNavigate();
-    const [ contact, setContact ] = useState({});
+    const [ contact, setContact ] = useState({ name: '', phone: ''});
 
     const onChangeInput = (e) => { setContact({ ...contact, [e.target.name] : e.target.value }) }
     const onSubmit = (e) => {
@@ -16,9 +16,27 @@ export default () => {
         await fetch(...Api, {
             method: "POST",
             body: JSON.stringify(contact)
-        }); navigate("/");
+        });
+        navigate("/");
     }
     return (
-        
+        <>
+            <h2> Agregar contacto </h2>
+            <div className="center">
+                <form className="form" onSubmit={onSubmit}>
+                    <label>
+                        <p>Name</p>
+                        <input type="text" name="name" onChange={onChangeInput} /> 
+                    </label>
+                    <label>
+                        <p>Phone</p>
+                        <input type="text" name="phone" onChange={onChangeInput} />                        
+                    </label>
+                    <div className="center">
+                        <button className="button primary"> Crear </button>
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
