30877ab924299b52aa0a7fc5757fb25214eca3d9
Michael Antony <146449026+Mitchel2003@users.noreply.github.com>
2024-07-12 00:16:45 -0500

    commit

--- src/components/pages/DetailsContact.jsx
+++ src/components/pages/DetailsContact.jsx
@@ -1,4 +1,38 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { Link  } from 'react-router-dom;
+import Contact from '../Contact';
+
 import '../styles/DetailsContact.css'
+
+export default () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [contact, setContact] = useState({});
+
+    useEffect(() => { loadContact() }, []);
+
+    const loadContact = async () => {
+        const res = await fetch(API_URL + id);
+        if(!res.ok) return navigate("/");
+        const json = await res.json();
+        setContact(json);
+    };
+    const deleteContact = async () => {
+        const req = await fetch(API_URL + id, { method: "DELETE" });
+        navigate("/");
+    };
+
+    return (
+        <>
+            <h2> Detalles contacto - { id } </h2>
+            <Contact {...contact} />
+            <div className="actions">
+                <button className="button danger" onClick={deleteContact}> Eliminar contacto </button>
+                <Link to={`/contact/${id}/edit`>
+                    <button className="button warning"> Editar contacto </button>
+                </Link>
+            </div> 
+        </>
+    )
+}
